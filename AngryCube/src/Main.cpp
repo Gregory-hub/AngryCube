b4518d3a90d7a9e4baa4bec1cbbd1174250a0733
Gregory-Hub <novikov.gri@gmail.com>
2025-08-09 12:45:56 +0300

    Setup OpenGL

---
+++ AngryCube/src/Main.cpp
@@ -0,0 +1,38 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+int main()
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
