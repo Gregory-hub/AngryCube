9fc80a935f51c935470c640956ad0da07d86aa19
Gregory-Hub <novikov.gri@gmail.com>
2025-08-15 15:34:04 +0300

    Render quad

--- AngryCube/src/Main.cpp
+++ AngryCube/src/Main.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+
+std::string readFile(const char* filename)
+{
+    std::ifstream file(filename);
+    if (!file.is_open())
+    {
+        std::cout << "Warning: file not open (" << filename << ")" << std::endl;
+        return "";
+    }
+    std::stringstream ss;
+    ss << file.rdbuf();
+    file.close();
+    return ss.str();
+}
 
 
 int main()
@@ -9,7 +29,20 @@ int main()
     if (!glfwInit())
         return -1;
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+    //GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    //const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    //glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    //glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    //glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    //glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+    //window = glfwCreateWindow(mode->width, mode->height, "My Title", monitor, NULL);
+
+    window = glfwCreateWindow(1280, 720, "Angry Cube", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -20,17 +53,82 @@ int main()
 
     if (glewInit() != GLEW_OK)
     {
+        glfwTerminate();
         return -1;
     }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float vertices[] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.5f, 0.5f,
+        -0.5f, 0.5f,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void*)0);
+
+    std::string vertShaderStr = readFile("shaders/basicColor.vert");
+    std::string fragShaderStr = readFile("shaders/basicColor.frag");
+
+    GLuint shaderProgram = glCreateProgram();
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertShaderSrc = vertShaderStr.c_str();
+    glShaderSource(vs, 1, &vertShaderSrc, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragShaderSrc = fragShaderStr.c_str();
+    glShaderSource(fs, 1, &fragShaderSrc, nullptr);
+    glCompileShader(fs);
+
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+    //glValidateProgram(shaderProgram);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
+        glBindVertexArray(vao);
+        glUseProgram(shaderProgram);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
